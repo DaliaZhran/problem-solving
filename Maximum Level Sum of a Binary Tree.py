d88c0063adc84f855c825672aa6a15507ea886a0
DaliaZhran <Dalia_zhran@hotmail.com>
2020-12-11 21:12:37 +0200

    changed structure

--- Maximum Level Sum of a Binary Tree.py
+++ Maximum Level Sum of a Binary Tree.py
@@ -65,3 +65,26 @@ class Solution(object):
         self.sums = []
         maxLevelSumHelper(root, 0)
         return 1 + self.sums.index(max(self.sums))
+
+
+# Time Complexity: O(N)
+# Space Complexity: O(N) because of stack call
+class Solution(object):
+    def maxLevelSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        level_sums = {}
+
+        def maxLevelSumDFS(node, level):
+            if not node:
+                return
+
+            maxLevelSumDFS(node.left, level + 1)
+            level_sums[level] = node.val + level_sums.get(level, 0)
+            maxLevelSumDFS(node.right, level + 1)
+
+        maxLevelSumDFS(root, 1)
+
+        return max(level_sums, key=level_sums.get)  # returns key and compares values

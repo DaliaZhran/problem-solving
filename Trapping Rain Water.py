6cd38ca9f2c15297bb4ad70217a93ad4e30dd0f8
DaliaZhran <Dalia_zhran@hotmail.com>
2020-12-21 02:38:03 +0200

    added dp solution

--- Trapping Rain Water.py
+++ Trapping Rain Water.py
@@ -4,7 +4,9 @@
 Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
 """
 
-
+# * Approach 1 : Two Pointers
+# Time -> O(N)
+# Space -> O(1)
 class Solution(object):
     def trap(self, height):
         """
@@ -30,6 +32,36 @@ class Solution(object):
                     answer += leftMax - height[left]
                 left += 1
         return answer
+
+
+# * Approach 2 : DP
+# Time -> O(N)
+# Space -> O(N)
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(height) - 1
+        max_left, max_right = 0, 0
+        water = 0
+
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] < max_left:
+                    water += max_left - height[left]
+                else:
+                    max_left = height[left]
+                left += 1
+            else:
+                if height[right] < max_right:
+                    water += max_right - height[right]
+                else:
+                    max_right = height[right]
+                right -= 1
+
+        return water
 
 
 # check https://leetcode.com/problems/trapping-rain-water-ii

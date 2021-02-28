d1ad53bb085cfa8447a9f9ea998a6005d10f7000
DaliaZhran <Dalia_zhran@hotmail.com>
2021-02-28 02:11:29 +0200

    27-2-2021

--- Leetcode V1/41. First Missing Positive.py
+++ Leetcode V1/41. First Missing Positive.py
@@ -1,3 +1,45 @@
+# https://leetcode.com/problems/first-missing-positive/
+"""
+Given an unsorted integer array nums, find the smallest missing positive integer.
+
+Example 1:
+
+Input: nums = [1,2,0]
+Output: 3
+
+Example 2:
+
+Input: nums = [3,4,-1,1]
+Output: 2
+"""
+
+# normal search -> faster than cyclic sort idk why -> it should be O(n^2) because "in" is O(n)
+def firstMissingPositive(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    if nums == []:
+        return 1
+    for i in range(1, len(nums) + 2):
+        if i not in nums:
+            return i
+
+
+# almost same as cyclic sort but with additional space
+
+
+def firstMissingPositive(self, nums):
+    nums_set = set()
+    for num in nums:
+        if num > 0:
+            nums_set.add(num)
+    curr = 1
+    while curr in nums_set and curr < len(nums) + 1:
+        curr += 1
+    return curr
+
+
 # cyclic sort
 class Solution(object):
     def firstMissingPositive(self, nums):
@@ -16,33 +58,6 @@ class Solution(object):
 
         for i in range(n):
             if nums[i] != i + 1:
-                return i+1
+                return i + 1
 
         return n + 1
-
-
-# normal search -> faster idk why -> it should be O(n^2) because "in" is O(n)
-def firstMissingPositive(self, nums):
-    """
-    :type nums: List[int]
-    :rtype: int
-    """
-    if nums == []:
-        return 1
-    for i in range(1, len(nums)+2):
-        if i not in nums:
-            return i
-
-
-# almost same as cyclic sort but with additional space
-
-
-def firstMissingPositive(self, nums):
-    nums_set = set()
-    for num in nums:
-        if num > 0:
-            nums_set.add(num)
-    curr = 1
-    while curr in nums_set and curr < len(nums)+1:
-        curr += 1
-    return curr

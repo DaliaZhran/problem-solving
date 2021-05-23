a2b2fa11a75a40500fd6bf5c939fbcce03b4aa73
DaliaZH <Dalia_zhran@hotmail.com>
2021-05-23 03:37:42 +0200

    tree

--- Binary Tree Right Side View.py
+++ Binary Tree Right Side View.py
@@ -1,39 +1,41 @@
+# https://leetcode.com/problems/binary-tree-right-side-view/
+"""
+Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+"""
+
+
 from collections import deque
+from typing import List
+
 
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 # Iterative BFS
 # Time complexity: O(N) since one has to visit each node.
 # Space complexity: O(D) to keep the queues, where D is a tree diameter. Let's use the last level to estimate the queue size.
 # This level could contain up to N/2 tree nodes in the case of complete binary tree.
-class Solution(object):
-    def rightSideView(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[int]
-        """
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
         if not root:
             return []
-        result = []
-        queue = deque()
-        queue.append(root)
+        queue = deque([root])
+        res = []
         while queue:
-            levelSize = len(queue)
-            for i in range(levelSize):
-                el = queue.popleft()
-                if el.left:
-                    queue.append(el.left)
-                if el.right:
-                    queue.append(el.right)
-            result.append(el.val)
-
-        return result
+            level_size = len(queue)
+            for _ in range(level_size):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(node.val)
+        return res
 
 
 # DFS
